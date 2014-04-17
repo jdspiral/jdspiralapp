baf01de439ef9f462d68cdce5995301263e9ece3
Josh Hathcock <jdspiral@gmail.com>
2014-04-17 10:38:46 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jdspiralapp::Application.config.secret_key_base = 'ea113ca8ec56def88c14fd26e0917b4740c9bb7321d5f49bfbb86299ef1c43edbf6a7c4023a80462ddc0f9781811ae83aa5082a4d9a64e66b2db79f8595cfb9f'
